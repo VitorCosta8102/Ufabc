dc0f3fc78ba6544d9488bb55a0352907b3304a34
VitorCosta8102 <vitor.formando.eletrica@gmail.com>
2018-09-26 14:43:25 -0300

    Exercicio 5 adicionado e 4 arrumado

---
+++ AEDII-Algoritmos_e_Estruturas_de_Dados_II/Pratica1/ex4/casos_teste.h
@@ -0,0 +1,7 @@
+#include<stdlib.h>
+
+void teste_sequencial();
+
+void teste_binaria();
+
+void imprime_teste(int array[], int array_size,int key);
---
+++ AEDII-Algoritmos_e_Estruturas_de_Dados_II/Pratica1/ex4/main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "search.h"
+#include "casos_teste.h"
+
+int main() {
+	teste_sequencial();
+	teste_binaria();
+	return 0;
+}
